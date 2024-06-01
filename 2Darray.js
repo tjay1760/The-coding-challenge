39e1b2b9d54ae91b400f6b118d2f7ab70fe26f5c
tjay1760 <mcjthiongo@gmail.com>
2024-06-01 03:33:49 +0300

    unoptimized solution

--- 2Darray.js
+++ 2Darray.js
@@ -1,5 +1,4 @@
 let glassesArray = [];
-let glassSum =0; 
 let allGlasses = 0;
 const glassWidth = 3;
 const glassHeight =3;
@@ -14,9 +13,14 @@ const dataArray = [
   ];
   for (let i =0; i<dataArray.length;i++){
     for (let j=0; j<dataArray[i].length;j++){
+        let glassSum =0; 
         if(j+1-glassWidth>=0 && i+1-glassHeight>=0){
+           glassSum = dataArray[i-2][j-2]+dataArray[i-2][j-1]+dataArray[i-2][j]
+            +dataArray[i-1][j-1]
+            +dataArray[i][j-2]+dataArray[i][j-1]+dataArray[i][j]
             allGlasses ++
+            glassesArray.push(glassSum)
         }
     }
   }
-console.log(allGlasses)
+console.log(glassesArray.sort((a,b)=>(b-a))[0])
