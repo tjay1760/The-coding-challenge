30d69c748e0440735a571c098a601236d7b993c9
tjay1760 <mcjthiongo@gmail.com>
2024-06-02 17:36:14 +0300

    display method

--- abstract_classes.js
+++ abstract_classes.js
@@ -4,4 +4,9 @@ this. title = title
 this.price = price
 this.author = author
     }
+    display(){
+        console.log(`Title : ${this.title}`)
+        console.log(`Price : ${this.price}`)
+        console.log(`Author : ${this.author}`)
+    }
 }
