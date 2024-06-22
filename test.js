17a35e6f14c2b804d53851a86f564dcc99822ffc
tjay1760 <mcjthiongo@gmail.com>
2024-06-22 21:35:59 +0300

    initial sort

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
-console.log(Math.floor(Math.random()*5))
-const audio =new Audio();
-console.log(audio)
+let myArr = [23,4,56,7,87,2,3,5,6,78,"y","u"];
+let fistSort = myArr.sort();
+console.log(fistSort)
