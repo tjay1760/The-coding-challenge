e29a43d28480a883a6b71ca4145cd756dd453a37
tjay1760 <mcjthiongo@gmail.com>
2024-07-06 13:27:45 +0300

    array methods

--- test.js
+++ test.js
@@ -1,5 +1,3 @@
-let myArr = [23,4,56,7,87,2,3,5,6,78,"y","u"];
-let fistSort = myArr.sort();
-console.log(fistSort)
-let ascSort = myArr.sort((a,b)=>b-a)
-console.log(ascSort)
+const mySentence = "you haev bee taken to 5 yeast /c/2/#"
+regex = /\W/
+console.log(mySentence.split(regex).reverse().join(""))
