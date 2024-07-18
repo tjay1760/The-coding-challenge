a5070a462158fa674abc7f3266c5ef4fba9287fe
tjay1760 <mcjthiongo@gmail.com>
2024-07-18 14:49:55 +0300

    initialize object array

---
+++ roman.js
@@ -0,0 +1,22 @@
+const romanNumbers = [
+    { roman: "IV", integer: 4 },
+    { roman: "XII", integer: 12 },
+    { roman: "IX", integer: 9 },
+    { roman: "XVII", integer: 17 },
+    { roman: "XXI", integer: 21 },
+    { roman: "III", integer: 3 },
+    { roman: "XVI", integer: 16 },
+    { roman: "VIII", integer: 8 },
+    { roman: "XIV", integer: 14 },
+    { roman: "VII", integer: 7 },
+    { roman: "XXV", integer: 25 },
+    { roman: "XXX", integer: 30 },
+    { roman: "L", integer: 50 },
+    { roman: "C", integer: 100 },
+    { roman: "D", integer: 500 },
+    { roman: "M", integer: 1000 },
+    { roman: "MD", integer: 1500 },
+    { roman: "MM", integer: 2000 },
+    { roman: "MMM", integer: 3000 },
+    { roman: "MCMXCIV", integer: 1994 }
+];
