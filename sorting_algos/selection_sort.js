bc85ee81ff1028c5c1e58c0d19c9c784c005f92c
tjay1760 <mcjthiongo@gmail.com>
2024-07-29 18:07:16 +0300

    min initialization

---
+++ sorting_algos/selection_sort.js
@@ -0,0 +1,10 @@
+const numbersArray = [88, 33,  7, 48, 51, 85, 81,  6, 54, 27, 19,  2, 39, 44, 17, 97, 19,
+    68, 44, 68, 26, 48, 98, 25,  5, 94, 85, 34,  2, 56];
+    const selectionSort = (arr)=>{
+        let min = arr[0];
+for (let i=0;i<arr.length;i++){
+min = arr[i]<min?arr[i]:min
+
+}
+    }
+selectionSort(numbersArray)
