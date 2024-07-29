5aa65c5c80f3a8fd324933d653effa41870a307c
tjay1760 <mcjthiongo@gmail.com>
2024-07-29 22:48:26 +0300

    sort selection

--- sorting_algos/selection_sort.js
+++ sorting_algos/selection_sort.js
@@ -1,10 +1,24 @@
-const numbersArray = [88, 33,  7, 48, 51, 85, 81,  6, 54, 27, 19,  2, 39, 44, 17, 97, 19,
-    68, 44, 68, 26, 48, 98, 25,  5, 94, 85, 34,  2, 56];
+const numbersArray = [88, 33,  7, 48, 51, 85];
     const selectionSort = (arr)=>{
-        let min = arr[0];
 for (let i=0;i<arr.length;i++){
-min = arr[i]<min?arr[i]:min
+    let theIndex = i;
+    let min = arr[0];
+for (let j=i;j<arr.length;j++){
+    if (arr[j]<min){
+min = arr[j];
+theIndex= j
+
+    }
+    if(theIndex!==j){
+        let swap = arr[i];
+        arr[i] = arr[theIndex];
+        arr[theIndex] = swap;
+    }
+}
+
 
 }
+
+console.log(arr);
     }
-selectionSort(numbersArray)
+selectionSort(numbersArray);
