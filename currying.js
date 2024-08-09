6c00e857f77e32ef8f4ad0fefea6a4ae371d2662
tjay1760 <mcjthiongo@gmail.com>
2024-08-09 13:38:43 +0300

    clean code

--- currying.js
+++ currying.js
@@ -1,16 +1,6 @@
-// normal
-// function add(x, y) {
-//     return x + y;
-//   }
-  
-//   console.log(add(3, 4));
+
 const add = (x,y) => x + y;
 console.log(add (2,3))
-// curried
-// function add(x,y){
-//     return function (y){
-//         return x*y;
-//     }
-// }
+
 const curryadd = (x,y)=>y=>x+y;
 console.log(curryadd(2)(3));
