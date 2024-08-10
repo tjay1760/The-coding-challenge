481fbdebf041b576fd2a6934b79f0de405add315
tjay1760 <mcjthiongo@gmail.com>
2024-08-10 09:02:50 +0300

    currying application

--- currying.js
+++ currying.js
@@ -2,11 +2,22 @@
 const curryadd = (x,y)=>y=>x+y;
 const addOne = curryadd(1);
 const addFour = addOne(3);
-console.log(addFour );
+// console.log(addFour );
 // initial function
 const multiply =(x,y,z)=> x*y*z;
 // carryed
 const carryMulti =x=>y=>z=>x*y*z;
 const multiplyTwo = carryMulti(2);
 const multiplyThree = multiplyTwo(3)
-console.log(multiplyThree(5));
+// console.log(multiplyThree(5));
+
+function handleClick(action) {
+    return function(event) {
+      console.log(`Performing ${action} action on button click.`);
+    };
+  }
+  function handleClick2(action) {
+      console.log(`Performing ${action} action on button click.`);
+  }
+  handleClick2("save");
+  handleClick("delete")("del");
