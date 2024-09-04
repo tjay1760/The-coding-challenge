b355c5a13d55d4bd6d684172deacd2e22a716320
tjay1760 <mcjthiongo@gmail.com>
2024-09-04 19:46:20 +0300

    orking ccounter

--- test.js
+++ test.js
@@ -10,17 +10,29 @@ let cid = [
   ['TWENTY', 60],
   ['ONE HUNDRED', 100]
 ];
+const currencyUnits = [
+    ['PENNY', 0.01],
+    ['NICKEL', 0.05],
+    ['DIME', 0.10],
+    ['QUARTER', 0.25],
+    ['ONE', 1.00],
+    ['FIVE', 5.00],
+    ['TEN', 10.00],
+    ['TWENTY', 20.00],
+    ['ONE HUNDRED', 100.00]
+]
 let cash = 50;
 const change = cash - price;
-const changeArr = [];
+const changeObj = {};
 const theChange = (change, cid)=>{
-    for (let i =cid.length-1;i>=0;i--){
-        while (change>cid[i][1]){
-            console.log(`Subtracting ${cid[i][1]} change is ${change}`)
-change-= cid[i][1];
+    for (let i =currencyUnits.length-1;i>=0;i--){
+        while (change>currencyUnits[i][1]){
+            changeObj[currencyUnits[i][0]] = (changeObj[currencyUnits[i][0]]||0)+ Number([currencyUnits[i][1]])  
+change-= currencyUnits[i][1];
+cid[i][1]-= currencyUnits[i][1];
         }
-console.log(`loop ${i}`)
     }
-
+console.log(changeObj)
+console.log(cid)
 }
 theChange(change,cid)
