8a52a172731321c7d8b78cd5accf7332d7188b84
tjay1760 <mcjthiongo@gmail.com>
2024-09-04 13:02:54 +0300

    tests

--- test.js
+++ test.js
@@ -1,2 +1,26 @@
-const arraytest = Array(5).fill("x");
-console.log((107/4000)*100);
+let price = 21.87;
+let cid = [
+  ['PENNY', 1.01],
+  ['NICKEL', 2.05],
+  ['DIME', 3.1],
+  ['QUARTER', 4.25],
+  ['ONE', 90],
+  ['FIVE', 55],
+  ['TEN', 20],
+  ['TWENTY', 60],
+  ['ONE HUNDRED', 100]
+];
+let cash = 50;
+const change = cash - price;
+const changeArr = [];
+const theChange = (change, cid)=>{
+    for (let i =cid.length-1;i>=0;i--){
+        while (change>cid[i][1]){
+            console.log(`Subtracting ${cid[i][1]} change is ${change}`)
+change-= cid[i][1];
+        }
+console.log(`loop ${i}`)
+    }
+
+}
+theChange(change,cid)
