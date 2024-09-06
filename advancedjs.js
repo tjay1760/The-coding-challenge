8c8a68e878b0b424dd0fde39dc0eb1053e3abace
tjay1760 <mcjthiongo@gmail.com>
2024-09-06 22:27:54 +0300

    closures

--- advancedjs.js
+++ advancedjs.js
@@ -1,11 +1,16 @@
+let base = 2;
 const outer = ()=>{
     let counter = 0
     const inner = () =>{
-        counter++
-        console.log(counter)
+        counter++;
+        base+=5;
+        console.log(`base =${base} counter =${counter} `)
     } 
     return inner
+    // the return statement returns the function as well as its scope
 }
-outer();
+const fn = outer();
+fn()
+fn()
 
 
