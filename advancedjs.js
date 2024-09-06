9409dc112e6298775f8b61e5ef2dc9ea234ba7f0
tjay1760 <mcjthiongo@gmail.com>
2024-09-07 00:12:31 +0300

    currying

--- advancedjs.js
+++ advancedjs.js
@@ -1,16 +1,3 @@
-let base = 2;
-const outer = ()=>{
-    let counter = 0
-    const inner = () =>{
-        counter++;
-        base+=5;
-        console.log(`base =${base} counter =${counter} `)
-    } 
-    return inner
-    // the return statement returns the function as well as its scope
-}
-const fn = outer();
-fn()
-fn()
-
+const sum = (a) =>(b) =>(c)=>a+b+c;
+console.log(sum (1)(2)(3));
 
