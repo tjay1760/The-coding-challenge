d2f78d3ff7c48ca356a0a4aa060d26a88595c522
tjay1760 <mcjthiongo@gmail.com>
2024-09-07 00:32:41 +0300

    invocation

--- advancedjs.js
+++ advancedjs.js
@@ -1,8 +1,16 @@
-
+function sayMyName (name){
+    console.log(`my name is ${this.name}`)
+}
+const sayMyName2 =(name)=>{
+    console.log(`my name is ${this.name}`)
+}
 const person ={
     name:"tjay",
     sayMyName: function (name){
         console.log(this.name)
     }
 }
-person.sayMyName()
+sayMyName2.call(person)
+
+// person.sayMyName()
+sayMyName.call(person)
