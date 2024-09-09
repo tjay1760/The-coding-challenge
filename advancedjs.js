82ad1afa8f55d84b0076cd89b582ba8ce044c5a3
tjay1760 <mcjthiongo@gmail.com>
2024-09-09 18:43:21 +0300

    class inheritance'

--- advancedjs.js
+++ advancedjs.js
@@ -7,3 +7,17 @@ class Person {
     console.log(`this is ${fname}  ${lname}`)
   }
 }
+class SuperHero extends Person {
+    
+    constructor(fname, lname){
+        super(fname,lname)
+        this.superhero = true;
+    }
+sayhero(){
+    if(this.superhero){
+        console.log(`${this.fname}  ${this.lname} is a super hero`)
+    }
+}
+}
+const widow = new SuperHero('black',"widow") 
+widow.sayhero()
