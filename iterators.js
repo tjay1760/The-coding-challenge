9657fb848f37b304dfb77b47972b7b3f20aa62e7
tjay1760 <mcjthiongo@gmail.com>
2024-09-09 23:28:24 +0300

    iterators start

---
+++ iterators.js
@@ -0,0 +1,12 @@
+const numbers = ["a","b","c","d","e"]
+const obj ={
+    1:"one",
+    2:"two",
+    3:"three",
+    4:"four",
+    5:"five"
+}
+for (const num in obj){
+    console.log(num)
+}
+// numbers.forEach((num)=>console.log(num))
