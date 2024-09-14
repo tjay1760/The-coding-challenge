71660e169b4e84975b055a28f731e72f506966cf
tjay1760 <mcjthiongo@gmail.com>
2024-09-14 16:43:08 +0300

    lookbehind

--- lookaround.js
+++ lookaround.js
@@ -1,5 +1,5 @@
 const str1 =
   "this sentence checks the order ID900 and the ID866 hich were not satisfied while they were submited to the customer order #555 as still qualified wth # 345 for $555 and $233";
 
-const test = str1.match(/\d+(?!\s)/gi);
+const test = str1.match(/(?<=ID)\d+/gi);
 console.log(test);
