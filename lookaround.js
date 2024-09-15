84831deb7fc8f38dd76c57bf3f692a3a0b8bd3af
tjay1760 <mcjthiongo@gmail.com>
2024-09-15 22:52:46 +0300

    practice

--- lookaround.js
+++ lookaround.js
@@ -1,5 +1,5 @@
 const str1 =
   "this sentence checks the orderID900 and theID866 hich were not satisfied while they were submited to the customer order #555 as still qualified wth # 345 for $555 and $233";
-
-const test = str1.match(/(?=e)en\w+/gi);
+const str2 = "x12xyx";
+const test = str2.match(/(?<=y)x/gi);
 console.log(test);
